ba626d5f5a6bc8adef82ce215dd0abf74bf33b9a
Matt Crump <crumpmj@gmail.com>
2019-09-03 07:35:45 -0400

    modify run_locally
    Added new message to display http://host:port
    
    Added host and port parameters to function

--- R/api_build.R
+++ R/api_build.R
@@ -4,14 +4,22 @@
 #' Run a jspsych experiment
 #'
 #' @param path path
+#' @param xprmntr_host host defaults to 127.0.0.1
+#' @param xprmntr_port port defaults to 8000
 #' @export
-run_locally <- function(path = ".") {
+run_locally <- function(path = ".",
+                        xprmntr_host = "127.0.0.1",
+                        xprmntr_port = 8000) {
 
   pr <- plumber::plumber$new()
 
   static_site <- file.path(path, "experiment")
   data_folder <- file.path(path, "data")
   static_router <- plumber::PlumberStatic$new(static_site)
+
+  # add message
+  message(paste("Point the browser to http://",xprmntr_host,":",xprmntr_port, sep=""))
+  #
 
   pr$mount("/", static_router)
   pr$handle("POST", "/submit", function(req, res){
@@ -25,5 +33,5 @@ run_locally <- function(path = ".") {
     readr::write_csv(dat, file.path(data_folder, paste0(file_id, ".csv")))
   })
 
-  pr$run(swagger = FALSE)
+  pr$run(swagger = FALSE, host=xprmntr_host, port= xprmntr_port)
 }
