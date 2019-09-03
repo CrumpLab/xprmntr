f90f22b8dac33f91ce1aa2c077e29a8b77166693
Matt Crump <crumpmj@gmail.com>
2019-09-03 08:54:49 -0400

    add to run_locally()
    add option to display in viewer or browser

--- R/api_build.R
+++ R/api_build.R
@@ -4,10 +4,12 @@
 #' Run a jspsych experiment
 #'
 #' @param path path
+#' @param show_in string, "viewer" to show in viewer, or "browser" to show in browser
 #' @param xprmntr_host host defaults to 127.0.0.1
 #' @param xprmntr_port port defaults to 8000
 #' @export
 run_locally <- function(path = ".",
+                        show_in = "viewer",
                         xprmntr_host = "127.0.0.1",
                         xprmntr_port = 8000) {
 
@@ -16,10 +18,6 @@ run_locally <- function(path = ".",
   static_site <- file.path(path, "experiment")
   data_folder <- file.path(path, "data")
   static_router <- plumber::PlumberStatic$new(static_site)
-
-  # add message
-  message(paste("Point the browser to http://",xprmntr_host,":",xprmntr_port, sep=""))
-  #
 
   pr$mount("/", static_router)
   pr$handle("POST", "/submit", function(req, res){
@@ -33,5 +31,16 @@ run_locally <- function(path = ".",
     readr::write_csv(dat, file.path(data_folder, paste0(file_id, ".csv")))
   })
 
+  # add message, and options to display in viewer or browser
+  message(paste("Point the browser to http://",xprmntr_host,":",xprmntr_port, sep=""))
+
+  if(show_in == "viewer"){
+    viewer <- getOption("viewer")
+    viewer(paste("http://",xprmntr_host,":",xprmntr_port, sep=""))
+  }
+  if(show_in == "browser") utils::browseURL(paste("http://",xprmntr_host,":",xprmntr_port, sep=""))
+  #
+
   pr$run(swagger = FALSE, host=xprmntr_host, port= xprmntr_port)
+
 }
